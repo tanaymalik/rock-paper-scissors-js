e8b07b3a243d63a0d0f05b9db2d9ffe18577d789
tanaymalik <tanaymalik06@gmail.com>
2024-02-14 23:44:29 +0530

    add ui to the game

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const options = ['rock', 'paper', 'scissors'];
+let playerCount = 0;
+let computerCount = 0;
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * options.length);
@@ -14,24 +16,56 @@ function gameRound(playerSelection, computerChoice) {
 
     if (!options.includes(playerSelection)) {
         console.log("Invalid input");
-    } else if (playerSelection === computerChoice) {
-        console.log(`It's a tie, ${playerSelection} and ${computerChoice} are the same.`);
+    }
+    else if (playerCount >= 5) {
+        result.textContent = "you won the entire game by gaining 5 points";
+    }
+    else if (computerCount >= 5) {
+        result.textContent = "you lost the game, as the computer has achieved 5 points";
+    }
+     else if (playerSelection === computerChoice) {
+        result.textContent = `It's a tie, ${playerSelection} and ${computerChoice} are the same.`;
+
     } else if (winConditions[playerSelection] === computerChoice) {
-        console.log(`You won, ${playerSelection} beats ${computerChoice}.`);
+        result.textContent = `You won, ${playerSelection} beats ${computerChoice}.`;
+        playerCount += 1;
     } else {
-        console.log(`You lose, ${computerChoice} beats ${playerSelection}.`);
+        result.textContent = `You lose, ${computerChoice} beats ${playerSelection}.`;
+        computerCount += 1;
     }
 }
 
 
 
-function game() {
-    for (let i = 1; i<=6; i++) {
-        let playerSelection = prompt("Please enter rock, paper, or scissors").toLowerCase();
-        let computerChoice = getComputerChoice();
-        gameRound(playerSelection, computerChoice);
-    }
+// function game() {
+//     for (let i = 1; i<=6; i++) {
+//         let playerSelection = prompt("Please enter rock, paper, or scissors").toLowerCase();
+//         let computerChoice = getComputerChoice();
+//         gameRound(playerSelection, computerChoice);
+//     }
+// }
+
+// game();
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const result = document.querySelector('.result');
+const current_score = document.querySelector('.current_score');
+
+
+rock.addEventListener('click', handleButtonClick);
+paper.addEventListener('click', handleButtonClick);
+scissors.addEventListener('click',  handleButtonClick);
+
+
+
+function handleButtonClick(event) {
+    const playerChoice = event.target.className;
+    const computerChoice = getComputerChoice();
+
+    gameRound(playerChoice, computerChoice);
+    current_score.textContent = `your score: ${playerCount}, computer Score: ${computerCount}`
 }
-
-game();
-
+    
+handleButtonClick();
